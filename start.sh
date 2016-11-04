bfd7584c7bc172a9952381417d470a17adf126b2
Loïc Mathieu <loikeseke@gmail.com>
2016-11-04 17:40:44 +0100

    Update start.sh
    fix: start cmd

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 
 if [ "$1" = 'ftp' ]; then
  echo "Launching vsftp on ftp protocol"
- `/usr/sbin/vsftpd`
+ /usr/sbin/vsftpd
 fi
 
 if [ "$1" = 'ftps' ]; then
  echo "Launching vsftp on ftps protocol"
- `/usr/sbin/vsftpd /etc/vsftp/vsftp_ftps.conf`
+ /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps.conf
 fi
 
 if [ "$1" = 'ftps_tls' ]; then
  echo "Launching vsftp on ftps with TLS only protocol"
- `/usr/sbin/vsftpd /etc/vsftp/vsftp_ftps_tls.conf`
+ /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps_tls.conf
 fi
