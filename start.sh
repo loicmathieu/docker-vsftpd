d2f030eaf68938620017b011eef88a7eb6100ed5
Loïc Mathieu <loikeseke@gmail.com>
2016-11-07 18:18:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,6 +13,11 @@ if [ "$1" = 'ftps' ]; then
  exec /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps.conf
 fi
 
+if [ "$1" = 'ftps_implicit' ]; then
+ echo "Launching vsftp on ftps protocol"
+ exec /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps_implicit.conf
+fi
+
 if [ "$1" = 'ftps_tls' ]; then
  echo "Launching vsftp on ftps with TLS only protocol"
  exec /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps_tls.conf
