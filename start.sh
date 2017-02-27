30a7dd2dd148362fb80c42ba8e815d99290c4007
Loïc Mathieu <loikeseke@gmail.com>
2017-02-27 17:14:35 +0100

    feat: refactor startup script + fix container stop itself in background mode

--- start.sh
+++ start.sh
@@ -1,37 +1,33 @@
 #!/bin/sh
 
+CONF_FILE = '/etc/vsftp/vsftp.conf'
+
 if [ "$1" = 'ftp' ]; then
  echo "Launching vsftp on ftp protocol"
- if [ -n "$PASV_ADDRESS" ]; then
-  echo "Activating passv on $PASV_ADDRESS"
-  echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftp/vsftp.conf
- fi
- exec /usr/sbin/vsftpd /etc/vsftp/vsftp.conf
+ CONF_FILE = '/etc/vsftp/vsftp.conf'
 fi
 
 if [ "$1" = 'ftps' ]; then
  echo "Launching vsftp on ftps protocol"
- if [ -n "$PASV_ADDRESS" ]; then
-  echo "Activating passv on $PASV_ADDRESS"
-  echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftp/vsftp.conf
- fi
- exec /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps.conf
+ CONF_FILE = '/etc/vsftp/vsftp_ftps.conf'
 fi
 
 if [ "$1" = 'ftps_implicit' ]; then
  echo "Launching vsftp on ftps protocol in implicit mode"
- if [ -n "$PASV_ADDRESS" ]; then
-  echo "Activating passv on $PASV_ADDRESS"
-  echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftp/vsftp.conf
- fi
- exec /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps_implicit.conf
+ CONF_FILE = '/etc/vsftp/vsftp_ftps_implicit.conf'
 fi
 
 if [ "$1" = 'ftps_tls' ]; then
- echo "Launching vsftp on ftps with TLS only protocol"
- if [ -n "$PASV_ADDRESS" ]; then
-  echo "Activating passv on $PASV_ADDRESS"
-  echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftp/vsftp.conf
- fi
- exec /usr/sbin/vsftpd /etc/vsftp/vsftp_ftps_tls.conf
+ echo "Launching vsftp on ftps with TLS only protocol" 
+ CONF_FILE = '/etc/vsftp/vsftp_ftps_tls.conf'
 fi
+
+if [ -n "$PASV_ADDRESS" ]; then
+  echo "Activating passv on $PASV_ADDRESS"
+  echo "pasv_address=$PASV_ADDRESS" >> $CONF_FILE
+ fi
+ 
+exec /usr/sbin/vsftpd $CONF_FILE
+
+# forever loop just to prevent Docker container to exit, when run as daemon
+while true; do sleep 1000; done
